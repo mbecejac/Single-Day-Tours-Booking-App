dd0bd7a7c11e035728f388cdd139fe20c3cbdac5
Marko Becejac <marko.becejac@gmail.com>
2023-02-05 10:14:21 +0100

    Initial commit: requirements and db connection

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+from db.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+
+def init_app():
+    app = FastAPI()
+
+    return app
+
+
+app = init_app()
+
+@app.get("/", include_in_schema=False)
+def hello_world():
+    return RedirectResponse('/docs')
+
+if __name__ == '__main__':
+    uvicorn.run(app)
